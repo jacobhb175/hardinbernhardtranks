3cc5ac45a9ca52a1dec2447904a3bcb07e44e9eb
jacobhb <jakehb2@gmail.com>
2020-06-09 12:15:52 -0400

    trying to set up firebase

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -12,9 +13,10 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDXM_FBLEWgMGLFMbV_nL9ScHBqsE2D9Dk",
     authDomain: "hardinbernhardtranks.firebaseapp.com",
     databaseURL: "https://hardinbernhardtranks.firebaseio.com",
@@ -24,6 +26,7 @@ var firebaseConfig = {
     appId: "1:498587943256:web:979cc52a0053224122a52d",
     measurementId: "G-FHXST7T8VQ"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
@@ -31,46 +34,44 @@ firebase.analytics();
 const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
-      const firebase = require("firebase");
-      // Required for side-effects
-      require("firebase/firestore");
 
-      // Initialize Cloud Firestore through Firebase
-      firebase.initializeApp({
-        apiKey: '### FIREBASE API KEY ###',
-        authDomain: '### FIREBASE AUTH DOMAIN ###',
-        projectId: '### CLOUD FIRESTORE PROJECT ID ###'
-      });
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: 'AIzaSyDXM_FBLEWgMGLFMbV_nL9ScHBqsE2D9Dk',
+    authDomain: 'hardinbernhardtranks.firebaseapp.com',
+    projectId: 'hardinbernhardtranks'
+});
+  
+var db = firebase.firestore();
 
-      var db = firebase.firestore();
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
 
-      db.collection("users").add({
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
-      })
-      .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
-      });
-      // Add a second document with a generated ID.
-      db.collection("users").add({
-        first: "Alan",
-        middle: "Mathison",
-        last: "Turing",
-        born: 1912
-      })
-      .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
-      });
+// Add a second document with a generated ID.
+db.collection("users").add({
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
 
-      db.collection("users").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          console.log(`${doc.id} => ${doc.data()}`);
-        });
-      });
+db.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
