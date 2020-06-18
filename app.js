f18d0751e7bec9124838b59f7a77d96cdf48e6b1
jacobhb <jakehb2@gmail.com>
2020-06-18 16:00:02 -0400

    ms teams

--- app.js
+++ app.js
@@ -2287,11 +2287,185 @@ db.collection("teams").doc("YonkersBJV").set({
     state:"New York",
     division:"Junior Varsity",
 rank:1200
-})
+}),
 
 
 //MS Teams
 
+db.collection("teams").doc("AlmadenMSA").set({
+    name:"Challenger-Almaden A",
+    state:"California",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("BRIGHTMSA").set({
+    name:"BRIGHT Homeschool A",
+    state:"New Jersey",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("BurleighManorAMS").set({
+    name:"Burleigh Manor A",
+    state:"Maryland",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("BurleighManorBMS").set({
+    name:"Burleigh Manor B",
+    state:"Maryland",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("BurleighManorCMS").set({
+    name:"Burleigh Manor C",
+    state:"Maryland",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("BurleighManorDMS").set({
+    name:"Burleigh Manor D",
+    state:"Maryland",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("CentennialLaneAMS").set({
+    name:"Centennial Lane ES A",
+    state:"Maryland",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("CentennialLaneBMS").set({
+    name:"Centennial Lane ES B",
+    state:"Maryland",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("CentennialLaneCMS").set({
+    name:"Centennial Lane ES C",
+    state:"Maryland",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("ClassicalCMS").set({
+    name:"Classical Prep C",
+    state:"Florida",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("ClassicalDMS").set({
+    name:"Classical Prep D",
+    state:"Florida",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("ClassicalEMS").set({
+    name:"Classical Prep E",
+    state:"Florida",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("CompassMSA").set({
+    name:"Compass A",
+    state:"Virginia",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("HarvestParkMSA").set({
+    name:"Harvest Park A",
+    state:"California",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("IndianFieldsMSA").set({
+    name:"Indian Fields A",
+    state:"New Jersey",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("JLSMSA").set({
+    name:"JLS A",
+    state:"California",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("NysmithAMS").set({
+    name:"Nysmith A",
+    state:"Virginia",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("NysmithBMS").set({
+    name:"Nysmith B",
+    state:"Virginia",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("RipponAMS").set({
+    name:"Rippon A",
+    state:"Virginia",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("RipponBMS").set({
+    name:"Rippon B",
+    state:"Virginia",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("StollerHMS").set({
+    name:"Stoller H",
+    state:"Washington",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("StollerKMS").set({
+    name:"Stoller K",
+    state:"Washington",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("StollerTMS").set({
+    name:"Stoller T",
+    state:"Washington",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("StollerWMS").set({
+    name:"Stoller W",
+    state:"Washington",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("TenaflyMSA").set({
+    name:"Tenafly Middle A",
+    state:"New Jersey",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("TenaflyMSB").set({
+    name:"Tenafly Middle B",
+    state:"New Jersey",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("TenaflyMSC").set({
+    name:"Tenafly Middle C",
+    state:"New Jersey",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("TrinityCMS").set({
+    name:"Trinity Prep C",
+    state:"Florida",
+    division:"Middle School",
+rank:1200
+}),
+db.collection("teams").doc("TrinityDMS").set({
+    name:"Trinity Prep D",
+    state:"Florida",
+    division:"Middle School",
+rank:1200
+})
 
 /*
 docRef.get().then(function(doc) {
@@ -2523,14 +2697,18 @@ async function newRank(A, B, AScore, BScore) {
     }
 };  
 
+//calculate and update rank
 async function nRank(A,B,AScore,BScore){
     console.log("nrank");
     rankA = rankA + q*K*((AScore/((AScore+BScore)*avgT))-expScoreA);
     rankA = rankB + q*K*((BScore/((AScore+BScore)*avgT))-expScoreB);
     console.log("rankAB",rankA,rankB);
+    //update rank
     db.collection("teams").doc(A).update({rank:rankA})
     db.collection("teams").doc(B).update({rank:rankB})
 }
+
+
 
 
 
---
+++ tournaments.js
@@ -0,0 +1,80 @@
+//Prelims
+//Round 1
+newRank(MissionSanJoseJV,SaratogaEJV,450,70);
+newRank(HomesteadAJV,MountainViewAJV,390,130);
+newRank(SaggitariusAJV,SantaClaraAJV,390,180);
+newRank(SaratogaDJV,CaliforniaAJV,340,210);
+newRank(HarkerAJV,EriksenBJV,370,150);
+newRank(AlmadenMSA,JLSMSA,360,200);
+newRank(LynbrookAV,LesQuizAV,450,90);
+newRank(PiedmontHillsAV,SaratogaCV,280,230);
+newRank(GundersonAV,EriksenAV,300,280);
+newRank(SaratogaAV,FoothillAV,440,190);
+newRank(PioneersAV,SaratogaBV,350,290);
+//Round 2
+newRank(LynbrookAV,SaratogaCV,490,80);
+newRank(PioneersAV,GundersonAV,430,210);
+newRank(FoothillAV,PiedmontHillsAV,310,300);
+newRank(HomesteadAJV,SaratogaEJV,300,150);
+newRank(SantaClaraAJV,HarkerAJV,320,250);
+newRank(MissionSanJoseJV,SaratogaDJV,330,270);
+newRank(SaggitariusAJV,MountainViewAJV,370,170);
+newRank(CaliforniaAJV,EriksenBJV,400,130);
+newRank(SaratogaBV,EriksenAV,340,310);
+newRank(SaratogaAV,LesQuizAV,540,90);
+newRank(AlmadenMSA,HarvestParkMSA,320,270);
+//Round 3
+newRank(SaratogaDJV,HomesteadAJV,330,210);
+newRank(SaggitariusAJV,SaratogaEJV,400,160);
+newRank(SantaClaraAJV,CaliforniaAJV,270,240);
+newRank(SaratogaCV,LesQuizAV,280,230);
+newRank(SaratogaAV,SaratogaBV,360,220);
+newRank(EriksenAV,PiedmontHillsAV,370,180);
+newRank(PioneersAV,LynbrookAV,370,210);
+newRank(GundersonAV,FoothillAV,260,240);
+newRank(MissionSanJoseJV,EriksenBJV,400,100);
+newRank(HarkerAJV,MountainViewAJV,360,120);
+newRank(JLSMSA,HarvestParkMSA,260,220);
+//Round 4
+newRank(SaratogaAV,GundersonAV,350,170);
+newRank(MissionSanJoseJV,SantaClaraAJV,340,240);
+newRank(PioneersAV,LesQuizAV,560,50)
+newRank(LynbrookAV,FoothillAV,390,200);
+newRank(HomesteadAJV,EriksenBJV,400,80);
+newRank(SaratogaBV,PiedmontHillsAV,440,140);
+newRank(CaliforniaAJV,MountainViewAJV,420,80);
+newRank(EriksenAV,SaratogaCV,440,150);
+newRank(AlmadenMSA,JLSMSA,410,160);
+newRank(HarkerAJV,SaratogaEJV,320,170);
+newRank(SaggitariusAJV,SaratogaDJV,330,180);
+//Round 5
+newRank(FoothillAV,LesQuizAV,380,130);
+newRank(SaratogaBV,GundersonAV,290,280);
+newRank(SaratogaAV,EriksenAV,320,290);
+newRank(CaliforniaAJV,SaratogaEJV,320,140);
+newRank(PioneersAV,SaratogaCV,490,110);
+newRank(LynbrookAV,PiedmontHillsAV,390,180);
+newRank(SaggitariusAJV,EriksenBJV,440,90);
+newRank(MissionSanJoseJV,MountainViewAJV,270,150);
+newRank(AlmadenMSA,HarvestParkMSA,390,230);
+newRank(SaratogaDJV,HarkerAJV,300,180);
+newRank(SantaClaraAJV,HomesteadAJV,300,170);
+//Round 6
+newRank(HarvestParkMSA,JLSMSA,270,150)
+//Varsity Playoffs
+//Semifinals
+newRank(PioneersAV,SaratogaBV,370,210);
+newRank(SaratogaAV,LynbrookAV,390,170);
+//Finals
+newRank(PioneersAV,SaratogaAV,360,230);
+//JV Playoffs
+//Semifinals
+newRank(SaggitariusAJV,HarkerAJV,310,170);
+newRank(MissionSanJoseJV,CaliforniaAJV,250,220);
+//Finals
+newRank(MissionSanJoseJV,SaggitariusAJV,280,210);
+//console.log("q "+q);
+teamAvgV = [];
+teamAvgJV = [];
+teamAvgMS = [];
+//logTR();
