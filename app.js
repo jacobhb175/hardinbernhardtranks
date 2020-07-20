0a28ffc5bdf47d21d1a6beda601ffc10a26e28cc
jacobhb <jakehb2@gmail.com>
2020-07-20 10:58:49 -0700

    fixing a typo

--- app.js
+++ app.js
@@ -16,7 +16,32 @@ server.listen(port, hostname, () => {
 */
 
 
-//--
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDXM_FBLEWgMGLFMbV_nL9ScHBqsE2D9Dk",
+    authDomain: "hardinbernhardtranks.firebaseapp.com",
+    databaseURL: "https://hardinbernhardtranks.firebaseio.com",
+    projectId: "hardinbernhardtranks",
+    storageBucket: "hardinbernhardtranks.appspot.com",
+    messagingSenderId: "498587943256",
+    appId: "1:498587943256:web:979cc52a0053224122a52d",
+    measurementId: "G-FHXST7T8VQ"
+};
+//firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+//const firebase = require("firebase");
+// Required for side-effects
+//require("firebase/firestore");
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: 'AIzaSyDXM_FBLEWgMGLFMbV_nL9ScHBqsE2D9Dk',
+    authDomain: 'hardinbernhardtranks.firebaseapp.com',
+    projectId: 'hardinbernhardtranks'
+});
+  
+var db = firebase.firestore();
 
 
 //Varsity Teams
@@ -3187,7 +3212,7 @@ db.collection("teams").doc("BerlinBJV").set({
     rank:1200,
     games:0
 }),
-db.collection("teams").doc("Loyola AV").set({
+db.collection("teams").doc("LoyolaAV").set({
     name:"Loyola A",
     state:"New York",
     division:"Varsity",
