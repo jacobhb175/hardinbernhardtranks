44c070cb13899d7f88dff904ab54372f7ddaee86
jacobhb <jakehb2@gmail.com>
2020-09-28 16:38:43 -0400

    Revert "something"
    This reverts commit 395effb9ce88b632bb67f70e4e594b13d5e24af8.

--- app.js
+++ app.js
@@ -17,44 +17,7 @@ server.listen(port, hostname, () => {
 
 //export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/service-account-file.json"
 
-var admin = require("firebase-admin");
 
-var serviceAccount = require("serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://hardinbernhardtranks.firebaseio.com"
-});
-
-const db = admin.firestore();
-
-/*
-//Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDXM_FBLEWgMGLFMbV_nL9ScHBqsE2D9Dk",
-    authDomain: "hardinbernhardtranks.firebaseapp.com",
-    databaseURL: "https://hardinbernhardtranks.firebaseio.com",
-    projectId: "hardinbernhardtranks",
-    storageBucket: "hardinbernhardtranks.appspot.com",
-    messagingSenderId: "498587943256",
-    appId: "1:498587943256:web:979cc52a0053224122a52d",
-    measurementId: "G-FHXST7T8VQ"
-};
-//firebase.initializeApp(firebaseConfig);
-//firebase.analytics();
-
-//const firebase = require("firebase");
-// Required for side-effects
-//require("firebase/firestore");
-
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-    apiKey: 'AIzaSyDXM_FBLEWgMGLFMbV_nL9ScHBqsE2D9Dk',
-    authDomain: 'hardinbernhardtranks.firebaseapp.com',
-    projectId: 'hardinbernhardtranks'
-});
-  
-var db = firebase.firestore();*/
 
 
 
