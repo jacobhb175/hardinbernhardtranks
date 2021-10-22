518607c44eb2c4a7cca078cd48c7733827fd2ff6
Jacob Hardin-Bernhardt <jakehb2@gmail.com>
2021-10-22 13:03:51 -0400

    test files

---
+++ qbranks/testswitcher.js
@@ -0,0 +1,56 @@
+switch(A){
+    case "Ridgewood":
+        B = RidgewoodA;
+        break;
+    case "East Brunswick":
+        B = EastBrunswickA;
+        break;
+    case "High Tech":
+        B = HighTechA;
+        break;
+    case "Woodbridge":
+        B = WoodbridgeA;
+        break;
+    case "Biotech A":
+        B = BiotechA;
+        break;
+    case "Biotech B":
+        B = BiotechB;
+        break;
+    case "Hunter A":
+        B = HunterA;
+        break;
+    case "Hunter B":
+        B = HunterB;
+        break;
+    case "River Dell A":
+        B = RiverDellA;
+        break;
+    case "River Dell B":
+        B = RiverDellB;
+        break;
+    case "Middlesex County Academy A":
+        B = MiddlesexCountyAcademyA;
+        break;
+    case "Middlesex County Academy B":
+        B = MiddlesexCountyAcademyB;
+        break;
+    case "Ithaca A":
+        B = IthacaA;
+        break;
+    case "Ithaca B":
+        B = IthacaB;
+        break;
+    case "Ithaca C":
+        B = IthacaC;
+        break;
+    case "John P. Stevens A":
+        B = JohnPStevensA;
+        break;
+    case "John P. Stevens B":
+        B = JohnPStevensB;
+        break;
+    case "John P. Stevens C":
+        B = JohnPStevensC;
+        break;
+}
