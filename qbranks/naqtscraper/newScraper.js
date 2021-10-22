b4ddf37ca0bf63465ae7996e6effe3a948deca4b
Jacob Hardin-Bernhardt <jakehb2@gmail.com>
2021-10-22 12:55:56 -0400

    other random web scraper edits

--- qbranks/naqtscraper/newScraper.js
+++
@@ -1,8 +0,0 @@
-const puppeteer = require('puppeteer');
-
-
-(async function scrape() {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://www.naqt.com/stats/tournament');
-})();
